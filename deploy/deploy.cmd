9e71129a4914fbff11c6160cf231f1932fc12524
Endre Bock <dev@itbock.de>
2015-08-23 22:01:19 +0200

    Use container to create app.

--- deploy/deploy.cmd
+++ deploy/deploy.cmd
@@ -7,6 +7,7 @@ echo Upload corejs...
 call gsutil cp -r ..\node_modules\corejs-w3c\src gs://page-manager.itbock.de/node_modules/corejs-w3c/
 echo Upload project...
 call gsutil cp -r ..\src gs://page-manager.itbock.de/
+call gsutil cp -r ..\config gs://page-manager.itbock.de/
 call gsutil cp -r ..\index.html gs://page-manager.itbock.de/index.html
 call gsutil cp -r ..\favicon.ico gs://page-manager.itbock.de/favicon.ico
 
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,32 +1,77 @@
 use.psr4("CoreJs", "node_modules/corejs-w3c/src");
 use.psr4("PageManager", "src");
 
-/* global PageManager */
-use("PageManager.Main");
-use("CoreJs.Bootstrap.HeadCleaner");
+use("CoreJs.DependencyInjection.Container");
 
 /**
  * Bootstrapper. 
+ * 
+ * @param {CoreJs.Ajax} ajax The ajax request object.
  */
-function Bootstrap() {
-	CoreJs.Event.Handler.call(this);
+function Bootstrap(ajax) {
+	CoreJs.Event.Listener.call(this);
+	this.ajax           = ajax;
+	this.windowReady    = false;
+	this.containerReady = false;
+	
+	this.addEventListener("pg.start", this);
 }
-Bootstrap.prototype = Object.create(CoreJs.Event.Handler.prototype);
+Bootstrap.prototype = Object.create(CoreJs.Event.Listener.prototype);
 Bootstrap.prototype.construct = Bootstrap;
 
-Bootstrap.prototype.handleEvent = function() {
-	// unbind
-	window.removeEventListener("load", this);
-	
-	// load & start application
-	namespace("PageManager", function() {
-		var cleaner = new CoreJs.Bootstrap.HeadCleaner(document.head);
-		this.app    = new PageManager.Main();
-		
-		cleaner.clean();
-		this.app.run();
-	});
+/**
+ * The event handler.
+ * 
+ * @param {Event} event System and custom events.
+ */
+Bootstrap.prototype.handleEvent = function(event) {
+	switch(event.type) {
+		case "load":
+			// unbind
+			window.removeEventListener(event.type, this);
+			this.windowReady = true;
+			this.dispatchEvent(new CoreJs.Event("pg.start"));
+		break;
+		case CoreJs.Ajax.Event.LOAD:
+			this.ajax.removeEventListener(CoreJs.Ajax.Event.LOAD, this);
+			var config = JSON.parse(event.detail.responseText);
+			var self = this;
+			namespace("PageManager.Injection", function() {
+				this.container = 
+					new CoreJs.DependencyInjection.Container(config);
+				this.container.load();
+				self.containerReady = true;
+				self.dispatchEvent(new CoreJs.Event("pg.start"));
+			});
+		break;
+		case "pg.start":
+			if (!this.windowReady || !this.containerReady) {
+				return;
+			}
+			/* global PageManager */
+			// start application
+			namespace("PageManager", function() {
+				var cleaner = 
+					PageManager.Injection.container.get("boot.cleaner");
+				this.app    =
+					PageManager.Injection.container.get("main");
+				
+				cleaner.clean();
+				this.app.run();
+			});
+		break;
+	}
 };
 
-var loader = new Bootstrap();
+/**
+ * Start loading activities.
+ */
+Bootstrap.prototype.load = function() {
+	this.ajax.addEventListener(CoreJs.Ajax.Event.LOAD, this);
+	this.ajax.load();
+};
+
+// Main routine
+var loader = new Bootstrap(new CoreJs.Ajax("get", "config/services.json"));
+loader.load();
 window.addEventListener("load", loader);
